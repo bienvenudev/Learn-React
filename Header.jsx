f1027311b9d71514c58407c9c2ab179d4d6ce557
bienvenudev <cbienvenu007@gmail.com>
2025-06-19 22:14:01 +0200

    feat: create App component and structure with Navbar and Main components

--- Header.jsx
+++ Header.jsx
@@ -1,8 +1,8 @@
 export default function Header() {
-    return (
-        <header className="header">
-            <img src="images/react-logo.png" className="nav-logo" alt="React logo" />
-            <div>ReactFacts</div>
-        </header>
-    )
+  return (
+    <header className="header">
+      <img src="images/react-logo.png" className="nav-logo" alt="React logo" />
+      <div>ReactFacts</div>
+    </header>
+  );
 }
---
+++ components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+   return (
+        <header className="header">
+        <nav>
+            <img src="images/react-logo.png" className="nav-logo" alt="React logo" />
+            <div>ReactFacts</div>
+        </nav>
+        </header>
+    )
+}
