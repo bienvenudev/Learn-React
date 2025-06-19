c9d98a8ea2b71a1edce4f0a38ab5989ab5451a58
bienvenudev <cbienvenu007@gmail.com>
2025-06-19 21:00:25 +0200

    feat: update Header component and add MainContent with styling adjustments

--- Header.jsx
+++ Header.jsx
@@ -1,14 +1,8 @@
 export default function Header() {
     return (
         <header className="header">
-            <img src="https://static.cdnlogo.com/logos/r/85/react.svg" className="nav-logo" alt="React logo" />
-            <nav>
-                <ul className="nav-list">
-                    <li className="nav-list-item">Pricing</li>
-                    <li className="nav-list-item">About</li>
-                    <li className="nav-list-item">Contact</li>
-                </ul>
-            </nav>
+            <img src="images/react-logo.png" className="nav-logo" alt="React logo" />
+            <div>ReactFacts</div>
         </header>
     )
 }
