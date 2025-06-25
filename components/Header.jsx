d1a9196a325bde53c32b49b0b146814cd05244a4
bienvenudev <cbienvenu007@gmail.com>
2025-06-25 17:50:57 +0200

    feat: add Header component and images for travel journal

---
+++ components/Header.jsx
@@ -0,0 +1,12 @@
+import { FaGlobe } from "react-icons/fa";
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        <FaGlobe width={6} />
+        <h1>my travel journal.</h1>
+      </header>
+    </>
+  );
+}
