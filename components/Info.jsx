0a76431d9f1e6111b5d28e9d8caa19a09e4eccc1
bienvenudev <cbienvenu007@gmail.com>
2025-06-27 11:44:34 +0200

    feat: update Info and Interests components

--- components/Info.jsx
+++ components/Info.jsx
@@ -4,7 +4,12 @@ import { FaLinkedin } from "react-icons/fa";
 export default function Info() {
   return (
     <div className="text-center mt-4 rounded-xl">
-      <img className="rounded-t-xl" src="../images/bienvenu.jpg" alt="Bienvenu's Portrait" width={320} />
+      <img
+        className="rounded-t-xl"
+        src="../images/bienvenu.jpg"
+        alt="Bienvenu's Portrait"
+        width={320}
+      />
       <h1 className="text-xl font-bold mt-5">Bienvenu Cyuzuzo</h1>
       <p className="text-sm text-[#F3BF99]">Frontend Developer</p>
       <a
@@ -15,16 +20,23 @@ export default function Info() {
       </a>
 
       <div className="flex gap-8 justify-center mt-4">
-       <button className="flex gap-2 items-center py-2 px-7 rounded-sm bg-white text-[#374151] cursor-pointer hover:opacity-80">
+        <a
+          href="mailto:cbienvenu007@gmail.com"
+          className="flex gap-2 items-center py-2 px-7 rounded-sm bg-white text-[#374151] cursor-pointer hover:opacity-80"
+        >
           {/* <span>📧 </span> */}
           <MdEmail className="text-[#1E1F26]" />
           Email
-        </button>
+        </a>
 
-        <button className="flex gap-2 items-center py-2 px-7 rounded-sm bg-[#5093E2] text-white cursor-pointer hover:opacity-80">
+        <a
+          href="https://www.linkedin.com/in/bienvenu-cyuzuzo/"
+          target="_blank"
+          className="flex gap-2 items-center py-2 px-7 rounded-sm bg-[#5093E2] text-white cursor-pointer hover:opacity-80"
+        >
           <FaLinkedin />
           Linkedin
-        </button>
+        </a>
       </div>
     </div>
   );
--- components/Interests.jsx
+++ components/Interests.jsx
@@ -3,8 +3,7 @@ export default function Interests() {
     <section className="pb-6">
       <h2 className="font-bold mt-8 mb-1">Interests</h2>
       <p className="font-normal text-[.8em] text-[#DCDCDC]">
-        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
-        Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+        Football. Music. Podcast. Psychology.
       </p>
     </section>
   );
