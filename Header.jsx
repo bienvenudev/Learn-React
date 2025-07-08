142166843e1050e36d4a82802893ec3edd854c2e
bienvenudev <cbienvenu007@gmail.com>
2025-07-08 16:36:32 +0200

    chore: reorganize cleanup

--- Header.jsx
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-  return (
-    <header className="header">
-      <img src="images/react-logo.png" className="nav-logo" alt="React logo" />
-      <div>ReactFacts</div>
-    </header>
-  );
-}
